1b95b5b1ea4f8df0182405243750b59dec8007ff
yanqirenshi <yanqirenshi@gmail.com>
2025-01-26 22:10:33 +0900

    var 0.0.15

--- dist/components/Manipulater.js
+++ dist/components/Manipulater.js
@@ -1,23 +1,20 @@
 "use strict";
 
-function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = Manipulater;
-var React = _interopRequireWildcard(require("react"));
+var _react = _interopRequireDefault(require("react"));
 var _Root = _interopRequireDefault(require("./Operators/Root.js"));
 var _Branch = _interopRequireDefault(require("./Operators/Branch.js"));
 var _operator = _interopRequireDefault(require("../js/operator.js"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return null; var r = new WeakMap(), t = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(e) { return e ? t : r; })(e); }
-function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != _typeof(e) && "function" != typeof e) return { "default": e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && Object.prototype.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n["default"] = e, t && t.set(e, n), n; }
 function Manipulater(props) {
   var actions = props.actions;
   var operators = props.operators;
   var window_size = props.window_size;
   var leafs = props.leafs;
-  React.useEffect(function () {
+  _react["default"].useEffect(function () {
     if (!window_size) return;
     if (!operators.initialized) {
       actions.operator.change(_operator["default"].initialize(window_size, operators));
@@ -30,10 +27,10 @@ function Manipulater(props) {
     return actions.operator.change(_operator["default"].clickedRoot(code, operators));
   };
   var selected_operator = _operator["default"].selectedOperator(operators);
-  return /*#__PURE__*/React.createElement(React.Fragment, null, selected_operator && /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(_Branch["default"], {
+  return /*#__PURE__*/_react["default"].createElement(_react["default"].Fragment, null, selected_operator && /*#__PURE__*/_react["default"].createElement(_react["default"].Fragment, null, /*#__PURE__*/_react["default"].createElement(_Branch["default"], {
     root: selected_operator
   }), leafs(selected_operator)), operators.list.map(function (operator) {
-    return /*#__PURE__*/React.createElement(_Root["default"], {
+    return /*#__PURE__*/_react["default"].createElement(_Root["default"], {
       key: operator.code,
       operator: operator,
       active: operator.code === operators.active,
--- dist/components/Operators/Root.js
+++ dist/components/Operators/Root.js
@@ -1,11 +1,10 @@
 "use strict";
 
-function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = Root;
-var React = _interopRequireWildcard(require("react"));
+var _react = _interopRequireDefault(require("react"));
 var _Box = _interopRequireDefault(require("@mui/material/Box"));
 var _Typography = _interopRequireDefault(require("@mui/material/Typography"));
 var _Psychology = _interopRequireDefault(require("@mui/icons-material/Psychology"));
@@ -18,8 +17,6 @@ var _SupportAgent = _interopRequireDefault(require("@mui/icons-material/SupportA
 var _Tooltip = _interopRequireDefault(require("@mui/material/Tooltip"));
 var _Work = _interopRequireDefault(require("@mui/icons-material/Work"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return null; var r = new WeakMap(), t = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(e) { return e ? t : r; })(e); }
-function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != _typeof(e) && "function" != typeof e) return { "default": e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && Object.prototype.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n["default"] = e, t && t.set(e, n), n; }
 function Root(props) {
   var operator = props.operator;
   var onClick = props.onClick;
@@ -54,22 +51,22 @@ function Root(props) {
   };
   var val = operator.label.val;
   var type = operator.label.type;
-  return /*#__PURE__*/React.createElement(_Box["default"], {
+  return /*#__PURE__*/_react["default"].createElement(_Box["default"], {
     sx: sx_base,
     onClick: click
-  }, type === 'function' && /*#__PURE__*/React.createElement(_Box["default"], {
+  }, type === 'function' && /*#__PURE__*/_react["default"].createElement(_Box["default"], {
     sx: sx_content
-  }, val()), type === 'text' && /*#__PURE__*/React.createElement(_Box["default"], {
+  }, val()), type === 'text' && /*#__PURE__*/_react["default"].createElement(_Box["default"], {
     sx: sx_content
   }, text(val, active)), type === 'icon' && icon(operator, active, val, sx_content));
 }
 function icon(operator, active, val, sx) {
-  if (active) return /*#__PURE__*/React.createElement(_Box["default"], {
+  if (active) return /*#__PURE__*/_react["default"].createElement(_Box["default"], {
     sx: sx
   }, iconContents(operator, active));
-  return /*#__PURE__*/React.createElement(_Tooltip["default"], {
+  return /*#__PURE__*/_react["default"].createElement(_Tooltip["default"], {
     title: val
-  }, /*#__PURE__*/React.createElement(_Box["default"], {
+  }, /*#__PURE__*/_react["default"].createElement(_Box["default"], {
     sx: sx
   }, iconContents(operator, active)));
 }
@@ -80,34 +77,34 @@ function iconContents(operator, active) {
     fontSize: 44,
     color: active ? 'rgba(8, 156, 163, 1.0)' : '#888'
   };
-  if ('settings' === code) return /*#__PURE__*/React.createElement(_Psychology["default"], {
+  if ('settings' === code) return /*#__PURE__*/_react["default"].createElement(_Psychology["default"], {
     sx: sx
   });
-  if ('search' === code) return /*#__PURE__*/React.createElement(_Search["default"], {
+  if ('search' === code) return /*#__PURE__*/_react["default"].createElement(_Search["default"], {
     sx: sx
   });
-  if ('backlogs' === code) return /*#__PURE__*/React.createElement(_Work["default"], {
+  if ('backlogs' === code) return /*#__PURE__*/_react["default"].createElement(_Work["default"], {
     sx: sx
   });
-  if ('github' === code) return /*#__PURE__*/React.createElement(_GitHub["default"], {
+  if ('github' === code) return /*#__PURE__*/_react["default"].createElement(_GitHub["default"], {
     sx: sx
   });
-  if ('links' === code) return /*#__PURE__*/React.createElement(_Link["default"], {
+  if ('links' === code) return /*#__PURE__*/_react["default"].createElement(_Link["default"], {
     sx: sx
   });
-  if ('customers' === code) return /*#__PURE__*/React.createElement(_SupportAgent["default"], {
+  if ('customers' === code) return /*#__PURE__*/_react["default"].createElement(_SupportAgent["default"], {
     sx: sx
   });
-  if ('meetings' === code) return /*#__PURE__*/React.createElement(_Groups["default"], {
+  if ('meetings' === code) return /*#__PURE__*/_react["default"].createElement(_Groups["default"], {
     sx: sx
   });
-  if ('products' === code) return /*#__PURE__*/React.createElement(_PrecisionManufacturing["default"], {
+  if ('products' === code) return /*#__PURE__*/_react["default"].createElement(_PrecisionManufacturing["default"], {
     sx: sx
   });
   return operator.label.val;
 }
 function text(text, active) {
-  return /*#__PURE__*/React.createElement(_Typography["default"], {
+  return /*#__PURE__*/_react["default"].createElement(_Typography["default"], {
     sx: {
       fontSize: 22,
       color: active ? 'rgba(8, 156, 163, 1.0)' : '#888'
